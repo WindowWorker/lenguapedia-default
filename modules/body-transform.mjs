17e601bf341f34d3f8042844fb33613d38325249
Patrick-ring-motive <60206943+Patrick-ring-motive@users.noreply.github.com>
2023-08-24 18:34:06 +0000

    /<body[^>]*>/,bodyTagHead+

--- modules/body-transform.mjs
+++ modules/body-transform.mjs
@@ -1,11 +1,11 @@
-import {addLangs} from './langs.mjs';
-import {csscalc} from './csscalc.mjs';
+import { addLangs } from './langs.mjs';
+import { csscalc } from './csscalc.mjs';
 await import('./preload-css.js');
-let cssmods=await(await fetch('https://files-servleteer.vercel.app/lenguapedia/default/mods.css')).text();
+let cssmods = await (await fetch('https://files-servleteer.vercel.app/lenguapedia/default/mods.css')).text();
 
 
-export default function transformBody(resBody, ct, hostList, hostProxy,xlangs,bkcolor) { 
-  cssmods.replaceAll('cce9ff',bkcolor);
+export default function transformBody(resBody, ct, hostList, hostProxy, xlangs, bkcolor) {
+  cssmods.replaceAll('cce9ff', bkcolor);
   const hostList_length = hostList.length;
   if (ct.includes('javascript')) {
     for (let i = 0; i < hostList_length; i++) {
@@ -13,15 +13,16 @@ export default function transformBody(resBody, ct, hostList, hostProxy,xlangs,bk
     }
   }
 
+  let bodyTagHead = resBody.match(/<body[^>]*>/)[0];
 
-  resBody = resBody.replace('<body>',
-    `<body>
-      <lenguapedia`+
+  resBody = resBody.replace(/<body[^>]*>/,
+    bodyTagHead +
+    `<lenguapedia` +
     `   host-list="` + btoa(JSON.stringify(hostList)) +
     `"  host-Proxy="` + btoa(hostProxy) +
-    `" xlangs="`+xlangs+`"></lenguapedia>`
-      +preloadCSS+                     
-      `<script src="/static/link-resolver.v.js?4"></script>
+    `" xlangs="` + xlangs + `"></lenguapedia>`
+    + preloadCSS +
+    `<script src="/static/link-resolver.v.js?4"></script>
       <script src="/static/inject-langs.js?2"></script>
       <link rel="stylesheet" href="/static/mods.css">
 `);
