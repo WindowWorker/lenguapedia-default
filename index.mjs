4f1e174a9e6ec473e0f5cada0b4ae29aa9377436
Patrick-ring-motive <60206943+Patrick-ring-motive@users.noreply.github.com>
2023-08-27 03:10:40 +0000

    Jjj

--- index.mjs
+++ index.mjs
@@ -152,7 +152,10 @@ let uselang = 'uselang='+langTo;
 
 let char='?';
   if(path.includes('?')){char='&';}
-  if(!path.includes('wapp')){path=path+char+uselang+'&'+translator;}
+  if(!path.includes('wapp')){
+   path=path+char;
+   path=path.split('?').join('?'+uselang)+translator;
+  }
   else{path = path+char+uselang; }
   let response = await tryURLs([hostTarget,hostIncubator,hostWiki,hostEn],path,hash,reqDTO);
   response = response||new Response();
